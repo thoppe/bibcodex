1e456b583fe24aa55d662509f99bf0b5276f1d24
thoppe <travis.hoppe@gmail.com>
2022-04-25 11:15:52 -0400

    Update reqs and version

--- bibcodex/_version.py
+++ bibcodex/_version.py
@@ -1,6 +1,7 @@
-__version__ = "1.0.2"
+__version__ = "1.1.0"
 
 """
+Version 1.1.0: Semantic embeddings (SPECTER) available.
 Version 1.0.2: Fix bug in validate. Progres bar not shown when empty.
 Version 1.0.1: Add tqdm status bar to downloads.
 Version 1.0.0: First release. Basic downloading enabled.
